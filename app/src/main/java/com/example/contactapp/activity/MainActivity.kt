63f32aa505c22f7e25afa5e86bd1dc49c432b6c2
HerrKarotte <dvp03ne@yandex.ru>
2025-05-03 18:00:09 +0300

    Ui: implement circular avatar in list cards

--- app/src/main/java/com/example/contactapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/contactapp/activity/MainActivity.kt
@@ -14,17 +14,22 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
@@ -39,8 +44,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
@@ -219,13 +227,32 @@ fun ContactCard(contact: Contact, onClick: () -> Unit) {
             .shadow(10.dp, shape = RoundedCornerShape(10.dp))
             .clickable(onClick = onClick)
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text(
-                text = contact.name, fontSize = 18.sp
-            )
-            Text(
-                text = contact.phone, fontSize = 18.sp
-            )
+        Row(
+            modifier = Modifier.padding(horizontal = 10.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(50.dp)
+                    .background(MaterialTheme.colorScheme.onPrimary)
+
+            ) {
+                Text(
+                    text = contact.name.take(1).uppercase(),
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = contact.name, fontSize = 18.sp
+                )
+                Text(
+                    text = contact.phone, fontSize = 18.sp
+                )
+            }
         }
     }
 }
